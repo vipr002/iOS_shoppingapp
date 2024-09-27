c74ab144760c061cb12801bb36579f060a59cd4c
Victoria Prigel <129780601+vipr002@users.noreply.github.com>
2024-09-27 17:41:06 +0200

    Initial Commit

---
+++ nettbutikk-styling/nettbutikk_stylingApp.swift
@@ -0,0 +1,17 @@
+//
+//  nettbutikk_stylingApp.swift
+//  nettbutikk-styling
+//
+//  Created by Victoria Prigel on 27/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct nettbutikk_stylingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

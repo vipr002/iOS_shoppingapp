e0efc50dbcb4568ee9a3c001132c41fa38b225c3
Victoria Prigel <vic.prigel@outlook.com>
2024-09-28 12:43:11 +0200

    Scrollable clothing
    Lagt til vertikal scroll i clothing category

--- nettbutikk-styling/ClothingView.swift
+++ nettbutikk-styling/ClothingView.swift
@@ -14,85 +14,95 @@ struct ClothingView: View {
     var body: some View {
         
         ZStack {
-
+            
             Color(UIColor(hex: "FFE9D2"))
             
-            VStack {
-                
-                HStack {
-                    
-                    Spacer()
-                    NavigationLink(destination: ClothingView()) {
-                        Text("Klær")
-                    }
-                    Spacer()
-                    Text("Sko")
-                        .onTapGesture {
-                            print("Sko tapped")
-                        }
-                    Spacer()
-                    Text("Tilbehør")
-                        .onTapGesture {
-                            print("Tilbehør tapped")
-                        }
-                    Spacer()
-                }
-                .padding()
-                .background(Color.white) // Bakgrunnsfarge for kategoriene
+            ScrollView(.vertical) {
                 
                 VStack {
                     
-                    ScrollView(.horizontal) {
+                    // ---- Kategorier øverst ----
+                    HStack {
                         
-                        HStack {
-                            
-                            ForEach($items) { $item in
-                                ItemCell(item: $item)
-                            }
+                        Spacer()
+                        NavigationLink(destination: ClothingView()) {
+                            Text("Klær")
                         }
+                        
+                        Spacer()
+                        Text("Sko")
+                            .onTapGesture {
+                                print("Sko tapped")
+                            }
+                        
+                        Spacer()
+                        Text("Tilbehør")
+                            .onTapGesture {
+                                print("Tilbehør tapped")
+                            }
+                        Spacer()
                     }
-                    .padding(.horizontal, 0.5)
+                    .padding()
+                    .background(Color.white)
                     
-                    ZStack {
-                        
-                        Color(UIColor(hex: "914920"))
-                            .frame(height: 400)
-                        
-                        VStack {
-                            
-                            Text("Høstens store trender")
-                                .foregroundColor(.white)
-                                .aspectRatio(contentMode: .fit)
-                                .font(.title)
-                                .padding(.top, 1)
-                            
-                            Image("blazer")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                            
-                            Button(action: {
-                                print("Nyhet button tapped")
-                            }) {
-                                Text("Nyheter")
-                                    .foregroundColor(.white)
-                                    .padding()
-                                    .frame(maxWidth: 130)
+                    // ----  Horistontale Items ---- 
+                    
+                    VStack {
+  
+                        ScrollView(.horizontal) {
+                            HStack {
+                                ForEach($items) { $item in
+                                    ItemCell(item: $item)
+                                }
                             }
-                            .background(Color.clear)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(Color.white, lineWidth: 1)
-                            )
-                            .padding(.top, 8)
-                            .padding(.trailing, 225)
+                        }
+                        .padding(.horizontal, 0.5)
+                        
+                        // --- Seksjon for høsttrender ---
+                        
+                        ZStack {
+                            
+                            Color(UIColor(hex: "914920"))
+                                .frame(height: 400)
+                            
+                            VStack {
+                                
+                                Text("Høstens store trender")
+                                    .foregroundColor(.white)
+                                    .aspectRatio(contentMode: .fit)
+                                    .font(.title)
+                                    .padding(.top, 1)
+                                
+                                Image("blazer")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                
+                                Button(action: {
+                                    print("Nyhet button tapped")
+                                }) {
+                                    Text("Nyheter")
+                                        .foregroundColor(.white)
+                                        .padding()
+                                        .frame(maxWidth: 130)
+                                }
+                                .background(Color.clear)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color.white, lineWidth: 1)
+                                )
+                                .padding(.top, 8)
+                                .padding(.trailing, 225)
+                            }
                         }
                     }
+                    
+                    Spacer()
                 }
-                Spacer()
             }
         }
     }
 }
+
 
 #Preview {
     ClothingView()
